42233a0a13cd7bb966db7ee68a84a92690a2529a
jitendra19 <jiten.cs2010@gmail.com>
2021-07-01 22:20:14 +0530

    efficient code for move zero to end in Array

--- 12.moveZerosToEndInArray.js
+++ 12.moveZerosToEndInArray.js
@@ -4,16 +4,26 @@ export default function moveZerosToEnd(input) {
     console.log('No element in Array');
     return 'No element in Array';
   }
+  // let maxIndex = 0;
+  // let zeroCount = 0;
+  // for (let i = 0; i < input.length; i++) {
+  //   if (input[i] === 0) {
+  //     zeroCount++;
+  //   } else {
+  //     input[maxIndex] = input[i];
+  //     maxIndex++;
+  //   }
+  // }
+  // input.fill(0, maxIndex, input.length - 1);
+  // console.log(`same array after move Zeros to end is ${input}`);
+
   let maxIndex = 0;
-  let zeroCount = 0;
   for (let i = 0; i < input.length; i++) {
-    if (input[i] === 0) {
-      zeroCount++;
-    } else {
+    if (input[i] !== 0) {
       input[maxIndex] = input[i];
+      input[i] = 0;
       maxIndex++;
     }
   }
-  input.fill(0, maxIndex, input.length - 1);
   console.log(`same array after move Zeros to end is ${input}`);
 }
