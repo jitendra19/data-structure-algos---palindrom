a2da060a4c0607d53e1bd0c20f201ffe49e7f667
jitendra19 <jiten.cs2010@gmail.com>
2021-08-09 23:01:10 +0530

    algo 24-3-4 isprime and primeFactors

--- 24.number-problems.js
+++ 24.number-problems.js
@@ -1,9 +1,14 @@
 export default function numberProblems() {
   // AllDivisors(12);
-  AllDivisorsInOrder(12);
+  // AllDivisorsInOrder(12);
+  // console.log(isPrime(13));
+  // console.log(isPrime(51));
+  // console.log(isPrime(251));
+  // console.log(isPrime(25));
+  // primeFactors(112);
+  primeFactors(251);
+  // primeFactors(127);
 }
-
-function primeFactor() {}
 
 function AllDivisors(num) {
   for (let i = 1; i * i <= num; i++) {
@@ -29,4 +34,27 @@ function AllDivisorsInOrder(num) {
       console.log(parseInt(num / i));
     }
   }
+}
+
+function isPrime(num) {
+  if (num === 1) return false;
+  if (num === 2 || num === 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function primeFactors(num) {
+  let manipulatedNum = num;
+  for (let i = 2; i * i <= manipulatedNum; i++) {
+    while (manipulatedNum % i === 0) {
+      console.log(i);
+      manipulatedNum = parseInt(manipulatedNum / i);
+    }
+  }
+  if (manipulatedNum > 1) console.log(manipulatedNum);
 }
