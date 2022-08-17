deba6bf4ff9435c97177c3b00ebf8fecef1eb861
jitendra19 <jiten.cs2010@gmail.com>
2022-08-17 23:30:30 +0530

    LeetCode - Convert Binary Number in a Linked List to Integer

--- 25LinkedList.js
+++ 25LinkedList.js
@@ -1,7 +1,7 @@
 // Linked list node
-function node(value, obj) {
+function node(value) {
   this.data = value;
-  this.next = obj;
+  this.next = null;
 }
 
 // creating head and adding trailling nodes
@@ -11,11 +11,6 @@ nod.next.next = new node(3, null);
 nod.next.next.next = new node(4, null);
 nod.next.next.next.next = new node(5, null);
 nod.next.next.next.next.next = new node(6, null);
-
-function node(value) {
-  this.data = value;
-  this.next = null;
-}
 
 function createLinkedList(nodes) {
   let linked1;
@@ -33,10 +28,6 @@ function createLinkedList(nodes) {
   return linked1;
 }
 
-const list = createLinkedList([1, 3, 4, 5, 6]);
-printLinkedList(list);
-console.log(list);
-
 function printLinkedList(linked1) {
   let counter = 0;
   while (linked1 && linked1.next !== null) {
@@ -45,6 +36,15 @@ function printLinkedList(linked1) {
   }
   console.log('linked list index - ', counter, 'value - ', linked1.data);
 }
+
+function deleteNode(linked1, index) {}
+function addNode(linked1, value, index) {}
+function updateNode(linked1, newValue, oldValue) {}
+
+// create linked list
+const list = createLinkedList([1, 3, 4, 5, 6]);
+// print linked list
+printLinkedList(list);
 
 // print linked list
 console.log('in given order');
@@ -68,3 +68,22 @@ function reverse(nod) {
 
 // calling reverse method
 reverse(nod);
+
+function getDecimalValue(head) {
+  let bin = '';
+  while (head !== null) {
+    bin = bin + head.data.toString();
+    head = head.next;
+  }
+  let val;
+  for (let i of bin) {
+    if (val === undefined) {
+      val = Number.parseInt(i);
+    } else {
+      val = val * 2 + Number.parseInt(i);
+    }
+  }
+  return val;
+}
+
+getDecimalValue(createLinkedList([1, 0, 1, 0, 1, 1]));
